107c382e13e6e07a1738184d681ed2f2bce32275
ericwang100 <ericwang2025@u.northwestern.edu>
2026-04-21 21:46:07 -0500

    Competition update/delete and descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_21_233000) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_21_234000) do
   create_table "competitions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.date "date"
+    t.text "description"
     t.string "name"
     t.integer "owner_id"
     t.datetime "updated_at", null: false
