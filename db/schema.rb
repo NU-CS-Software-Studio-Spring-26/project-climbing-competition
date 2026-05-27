1d11f3703d626bbd0194c176edf3fee4e8f1358d
hannahbanllana <hannahkwak2027@u.northwestern.edu>
2026-05-27 10:45:37 -0500

    adjusted tests to work with new auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_27_000002) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_27_000003) do
   create_table "attempts", force: :cascade do |t|
     t.integer "attempt_count", null: false
     t.integer "climb_id", null: false
